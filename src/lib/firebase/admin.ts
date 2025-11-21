b3f8f32f98eac4b823c1466921bbe2b32d35b2bf
Gabriel Amarilla <gaperalta.dev@gmail.com>
2025-11-21 09:53:11 -0300

    [env] More secure api keys injection by env variables

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -3,9 +3,9 @@ import * as admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
+      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     }),
   });
 }
