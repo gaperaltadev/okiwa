4f5dece59fc62c8e151d0f362a9a7039cb4de549
Gabriel Amarilla <gaperalta.dev@gmail.com>
2025-12-25 22:34:42 -0300

    [firebase] handle private key issue with vercel environment variables

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,9 +1,14 @@
 import * as admin from "firebase-admin";
 
+const privateKey = process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY?.replace(
+  /\\n/g,
+  "\n"
+);
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_PRIVATE_KEY,
+      privateKey: privateKey,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     }),
