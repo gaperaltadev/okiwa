1794405f1a8ed9c30da99d65452298c4f9af270d
Gabriel Amarilla <gaperalta.dev@gmail.com>
2025-12-25 22:00:29 -0300

    [firebase] Secure firebase api keys by using env variables

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpgCM-iq4U_n0ELXK4UST2SazbCYsEZOQ",
-  authDomain: "okiwa-dec5b.firebaseapp.com",
-  projectId: "okiwa-dec5b",
-  storageBucket: "okiwa-dec5b.firebasestorage.app",
-  messagingSenderId: "1029194026821",
-  appId: "1:1029194026821:web:bfdf90adba6344ad7e782c",
-  measurementId: "G-3TXP4MD9VT",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
