965cfcc918ac4bd2728641de889e80bd107e102c
Gabriel Amarilla <gaperalta.dev@gmail.com>
2025-12-26 20:03:23 -0300

    [hotfix] New mobile navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { useMemo } from "react";
 import {
   GroupOutlined,
@@ -9,8 +9,10 @@ import {
   LocalOfferOutlined,
   TrendingUpOutlined,
 } from "@mui/icons-material";
+import { Autocomplete, TextField } from "@mui/material";
 
 export const Navbar = () => {
+  const router = useRouter();
   const pathname = usePathname();
 
   const paths = useMemo(
@@ -41,22 +43,32 @@ export const Navbar = () => {
 
   return (
     <div
-      className={`${
-        pathname === "/sign-in" ? "hidden" : "flex"
-      } w-screen gap-2 p-4 justify-center items-center`}
+      className={`${pathname === "/sign-in" ? "hidden" : "flex"} w-screen p-4`}
     >
-      {paths.map((path, idx) => (
-        <Link key={`${path.label}-${idx}`} href={path.href}>
-          <div
-            className={`grid gap-2 rounded-xl px-4 py-2 ${
-              pathname === path.href ? "bg-black text-white" : ""
-            }`}
-          >
-            {path.icon}
-            <p className="font-bold text-lg">{path.label}</p>
-          </div>
-        </Link>
-      ))}
+      <div className="md:hidden w-full">
+        <Autocomplete
+          fullWidth
+          options={paths}
+          getOptionLabel={(option) => option.label}
+          onChange={(_, newValue) => router.push(newValue?.href || "/")}
+          value={paths.find((path) => path.href === pathname) || null}
+          renderInput={(params) => <TextField {...params} label="Navegar" />}
+        />
+      </div>
+      <div className="grid-cols-4 gap-2 hidden sm:grid">
+        {paths.map((path, idx) => (
+          <Link key={`${path.label}-${idx}`} href={path.href}>
+            <div
+              className={`grid gap-2 rounded-xl px-4 py-2 ${
+                pathname === path.href ? "bg-black text-white" : "border"
+              }`}
+            >
+              {path.icon}
+              <p className="font-bold text-lg">{path.label}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
